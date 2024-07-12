2a91706a802e2002efbd05415f97d10bad7f5dec
Márjory <135056687+marjNDS@users.noreply.github.com>
2024-07-12 12:15:28 -0300

    Update auth.py with comments

--- core/auth.py
+++ core/auth.py
@@ -17,58 +17,52 @@ from core.security import verificar_senha
 from pydantic import EmailStr
 
 
-# cria um endpoint para autenticação. utiliza o token de acesso
-
+# Cria um endpoint para autenticação, utilizando o token de acesso
 oauth2_schema = OAuth2PasswordBearer(
-
     tokenUrl=f"{settings.API_V1_STR}/usuarios/login"
-
 )
 
-
+# Função assíncrona para autenticar um usuário
 async def autenticar(email: EmailStr, senha: str, db: AsyncSession) -> Optional[UsuarioModel]:
-
+    # Cria uma sessão assíncrona com o banco de dados
     async with db as session:
-
+        # Define a consulta para selecionar o usuário com o e-mail fornecido
         query = select(UsuarioModel).filter(UsuarioModel.email == email)
-
+        # Executa a consulta
         result = await session.execute(query)
-
+        # Obtém o usuário único ou nenhum
         usuario: UsuarioModel = result.scalars().unique().one_or_none()
 
-        # se não achou o usuário retorna none
-
+        # Se o usuário não for encontrado, retorna None
         if not usuario:
-
             return None
 
-        # se as senhas não batem retorna none também
-
+        # Se a senha não corresponder, retorna None
         if not verificar_senha(senha, usuario.senha):
-
             return None
 
+        # Retorna o usuário se as credenciais estiverem corretas
         return usuario
 
-
+# Função privada para criar um token JWT
 def _criar_token(tipo_token: str, tempo_vida: timedelta, usuario: str) -> str:
+    payload = {}  # Dicionário para armazenar os dados do token
+    sp = timezone('America/Sao_Paulo')  # Define o fuso horário para São Paulo
+    expira = datetime.now(tz=sp) + tempo_vida  # Calcula a data de expiração do token
 
-    payload = {}
-    sp = timezone('America/Sao_Paulo')
-
-    expira = datetime.now(tz=sp) + tempo_vida
-
+    # Define os dados do token
     payload["type"] = tipo_token
     payload["exp"] = expira
     payload["iat"] = datetime.now(tz=sp)
     payload["sub"] = str(usuario)
 
+    # Codifica o token usando a chave secreta e o algoritmo especificado
     return jwt.encode(payload, settings.JWT_SECRET, algorithm=settings.ALGORITHM)
 
-
+# Função para criar um token de acesso
 def criar_token_acesso(sub: str) -> str:
     return _criar_token(
-        tipo_token='access_token',
-        tempo_vida=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
-        usuario=sub
+        tipo_token='access_token',  # Define o tipo do token como "access_token"
+        tempo_vida=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),  # Define o tempo de vida do token
+        usuario=sub  # Define o usuário associado ao token
     )
