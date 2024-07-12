e5f4528b4d1e57d141c0981d33561dc92efa582d
Márjory <marjory.23104708360036@faeterj-rio.edu.br>
2024-07-12 15:18:09 -0300

    models + artigo endpoint + my.db

---
+++ schemas/usuario_schema.py
@@ -0,0 +1,33 @@
+from typing import Optional, List
+
+from pydantic import BaseModel, EmailStr
+
+from schemas.artigo_schema import ArtigoSchema
+
+
+class UsuarioSchemaBase(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    sobrenome: str
+    email: EmailStr
+    is_admin: bool = False
+
+    class Config:
+        orm_mode = True
+
+# para criar uma conta
+class UsuarioSchemaCreate(UsuarioSchemaBase):
+    senha: str
+
+
+class UsuarioSchemaArtigos(UsuarioSchemaBase):
+    artigos: Optional[List[ArtigoSchema]]
+
+
+#para atualizar dados no bd
+class UsuarioSchemaUp(UsuarioSchemaBase):
+    nome: Optional[str]
+    sobrenome: Optional[str]
+    email: Optional[str]
+    senha: Optional[str]
+    is_admin: Optional[str]
